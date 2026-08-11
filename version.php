8be95e35f44342bdf2083dbea6f5a76b9a1cb770
LMS Labs Deploy <deploy@lms-labs.com>
2026-08-11 17:00:08 +0000

    release: mod_slideshow v1.6.30

--- version.php
+++ version.php
@@ -98,7 +98,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_slideshow';
-$plugin->version   = 2026072300235;
+$plugin->version   = 2026072300;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.6.30'; // FEAT: Video slides (YouTube/URL/upload) + poster image slides with position selection and watch gating.
